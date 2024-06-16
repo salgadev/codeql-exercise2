62b38799b8649fae4cdea3c3dc1fef8fb2bd715a
Carlos Salgado <socd06@gmail.com>
2024-06-16 13:49:19 -0400

    Add files via upload

---
+++ exercise.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>    
+
+#define BUFSIZE 256
+                                
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+                                
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
+    }
+                                
+    char buff[BUFSIZE];
+                                
+    while (fgets(buff, BUFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+        
+    return 0; 
+}
+
